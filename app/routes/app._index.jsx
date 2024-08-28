9248f012a7fdba14f5de0c57a68fc10c25d7eb24
岩弈 <oncwnuItq3UWm0BoCNj7YclFsdYs@git.weixin.qq.com>
2024-08-28 18:30:18 +0800

    i18n

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,11 +1,14 @@
 import {
   Page,
   Layout,
-  BlockStack,
+  BlockStack, InlineStack, Text, Button, Card,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 import { authenticate } from "../shopify.server";
-import SelectProducts from "../components/selectProducts.jsx";
+import {useState} from "react";
+import {useI18n} from "@shopify/react-i18n";
+import en from '../locales/en.json';
+import zhCN from '../locales/zh-CN.json';
 
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
@@ -16,11 +19,33 @@ export const loader = async ({ request }) => {
 
 
 export default function Index() {
+  const [i18n, ShareTranslations] = useI18n({
+    id: 'greeting',
+    fallback: en,
+    translations: {
+      en,
+      'zh-CN': zhCN
+    }
+  });
 
+  const [selectedProducts, setSelectedProducts] = useState([]);
+
+  async function selectProduct() {
+    const products = await window.shopify.resourcePicker({
+      type: "product",
+      action: 'add',
+      multiple: true
+    });
+
+    if (products) {
+      setSelectedProducts(products);
+    }
+  }
 
 
 
   return (
+
     <Page>
       <TitleBar title="Remix app template">
 
@@ -28,7 +53,31 @@ export default function Index() {
       <BlockStack gap="500">
         <Layout>
           <Layout.Section>
-            {/*<SelectProducts multiple={true} />*/}
+            <Card>
+              <BlockStack gap="500">
+                <InlineStack align="space-between">
+                  <Text as={"h2"} variant="headingLg">
+                   Products {i18n.translate('greeting')}
+                  </Text>
+                  {selectedProducts.length > 0 && (
+                    <Button variant="plain" onClick={selectProduct}>
+                      Change products
+                    </Button>
+                  )}
+                </InlineStack>
+                {selectedProducts.length > 0 ? (
+                  <InlineStack blockAlign="center" gap="500">
+                    Change product
+                  </InlineStack>
+                ) : (
+                  <BlockStack gap="200">
+                    <Button onClick={selectProduct} id="select-product">
+                      Select products
+                    </Button>
+                  </BlockStack>
+                )}
+              </BlockStack>
+            </Card>
           </Layout.Section>
           <Layout.Section variant="oneThird">
           </Layout.Section>
--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -2,30 +2,51 @@ import { json } from "@remix-run/node";
 import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
 import { boundary } from "@shopify/shopify-app-remix/server";
 import { AppProvider } from "@shopify/shopify-app-remix/react";
-import { NavMenu } from "@shopify/app-bridge-react";
+import {NavMenu, useAppBridge} from "@shopify/app-bridge-react";
 import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
 import { authenticate } from "../shopify.server";
+import {I18nManager, I18nContext} from "@shopify/react-i18n";
+import {useEffect, useState} from "react";
+
+
 
 export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
 
+
   return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
 };
 
 export default function App() {
   const { apiKey } = useLoaderData();
+  const app = useAppBridge();
+  const [locale, setLocale] = useState('en');
 
+  useEffect(() => {
+    console.log(app.config.locale)
+    setLocale(app.config.locale);
+
+  }, [app]);
+
+  const i18nManager = new I18nManager({
+    locale,
+    onError(error) {
+      boundary.error(error);
+    },
+  });
   return (
     <AppProvider isEmbeddedApp apiKey={apiKey}>
-      <NavMenu>
-        <Link to="/app" rel="home">
-          Home
-        </Link>
-        <Link to="/app/additional">Additional page</Link>
-      </NavMenu>
-      <Outlet />
+      <I18nContext.Provider value={i18nManager}>
+        <NavMenu>
+          <Link to="/app" rel="home">
+            Home
+          </Link>
+          <Link to="/app/additional">Additional page</Link>
+        </NavMenu>
+        <Outlet />
+      </I18nContext.Provider>
     </AppProvider>
   );
 }
