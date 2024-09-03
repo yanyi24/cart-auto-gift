6103dc937ad00a260956c108a631bb4f3bacdded
岩弈 <oncwnuItq3UWm0BoCNj7YclFsdYs@git.weixin.qq.com>
2024-09-03 18:30:27 +0800

    discount

---
+++ extensions/product-discount/src/run.js
@@ -0,0 +1,26 @@
+// @ts-check
+import { DiscountApplicationStrategy } from "../generated/api";
+
+/**
+ * @typedef {import("../generated/api").RunInput} RunInput
+ * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ */
+
+/**
+ * @type {FunctionRunResult}
+ */
+const EMPTY_DISCOUNT = {
+  discountApplicationStrategy: DiscountApplicationStrategy.First,
+  discounts: [],
+};
+
+/**
+ * @param {RunInput} input
+ * @returns {FunctionRunResult}
+ */
+export function run(input) {
+  const targets = input.cart.lines;
+  console.log("targets", targets)
+
+  return EMPTY_DISCOUNT;
+}
