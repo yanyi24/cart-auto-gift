62c3df6cfb3cf489910f4f774996ca86590ecbdd
岩弈 <oncwnuItq3UWm0BoCNj7YclFsdYs@git.weixin.qq.com>
2024-09-18 17:19:07 +0800

    整合之前

--- extensions/auto-gift-discount/src/run.js
+++ extensions/auto-gift-discount/src/run.js
@@ -67,22 +67,43 @@ export function run(input) {
   const getAllGiftVariants = (conditions) =>
     conditions.flatMap(c => c.products.flatMap(p => p.variants));
 
-  if (buys.type === 'ALL_PRODUCTS') {
-    const allGifts = getAllGiftVariants(conditions); // 获取所有礼品的商品变体ID
-
+  const handleRule = (lines) => {
     if (rule === 'QUANTITY') {
-      // 筛选出购物车中不包含礼品的商品
-      const linesExceptedGifts = cartLines.filter(line => !allGifts.includes(removeGidStr(line.merchandise.id)));
       // 根据筛选结果找到对应的条件
-      const condition = findObjectByValue(conditions, linesExceptedGifts.length);
+      const condition = findObjectByValue(conditions, lines.length);
       if (!condition.quantity) return EMPTY_DISCOUNT; // 如果条件不满足数量要求，返回空折扣
       return applyDiscount(condition, cartLines); // 应用折扣
     }
 
     if (rule === 'AMOUNT') {
       // 计算购物车中不包含礼品商品的总金额
-      const exceptedGiftsTotalAmount = cartLines
-        .filter(line => !allGifts.includes(removeGidStr(line.merchandise.id)))
+      const exceptedGiftsTotalAmount = lines
+        .reduce((total, line) => total + Number(line.cost.totalAmount.amount), 0);
+      // 根据总金额找到对应的条件
+      const condition = findObjectByValue(conditions, exceptedGiftsTotalAmount, 'amount');
+      if (!condition.amount) return EMPTY_DISCOUNT; // 如果条件不满足金额要求，返回空折扣
+      return applyDiscount(condition, cartLines); // 应用折扣
+    }
+  }
+
+  const allGifts = getAllGiftVariants(conditions); // 获取所有礼品的商品变体ID
+
+  if (buys.type === 'ALL_PRODUCTS') {
+    // 筛选出购物车中不包含礼品的商品
+    const excludedGiftLines = cartLines.filter(line => !allGifts.includes(removeGidStr(line.merchandise.id)));
+
+    // handleRule(excludedGiftLines)
+    if (rule === 'QUANTITY') {
+
+      // 根据筛选结果找到对应的条件
+      const condition = findObjectByValue(conditions, excludedGiftLines.length);
+      if (!condition.quantity) return EMPTY_DISCOUNT; // 如果条件不满足数量要求，返回空折扣
+      return applyDiscount(condition, cartLines); // 应用折扣
+    }
+
+    if (rule === 'AMOUNT') {
+      // 计算购物车中不包含礼品商品的总金额
+      const exceptedGiftsTotalAmount = excludedGiftLines
         .reduce((total, line) => total + Number(line.cost.totalAmount.amount), 0);
       // 根据总金额找到对应的条件
       const condition = findObjectByValue(conditions, exceptedGiftsTotalAmount, 'amount');
@@ -93,10 +114,10 @@ export function run(input) {
 
   if (buys.type === 'PRODUCTS') {
     const buysIds = buys.value.flatMap(p => p.variants); // 获取买入规则中的商品变体ID
+    // 筛选购物车中包含买入规则商品的商品
+    const linesIncludesBuys = cartLines.filter(line => buysIds.includes(removeGidStr(line.merchandise.id)));
 
     if (rule === 'QUANTITY') {
-      // 筛选购物车中包含买入规则商品的商品
-      const linesIncludesBuys = cartLines.filter(line => buysIds.includes(removeGidStr(line.merchandise.id)));
       // 根据筛选结果找到对应的条件
       const condition = findObjectByValue(conditions, linesIncludesBuys.length);
       if (!condition.quantity) return EMPTY_DISCOUNT; // 如果条件不满足数量要求，返回空折扣
@@ -104,10 +125,57 @@ export function run(input) {
     }
 
     if (rule === 'AMOUNT') {
-      // 计算购物车中买入规则商品的总金额
-      const buysTotalAmount = cartLines
-        .filter(line => buysIds.includes(removeGidStr(line.merchandise.id)))
+      // 计算购物车中符合买入规则商品的总金额
+      const buysTotalAmount = linesIncludesBuys
         .reduce((total, line) => total + Number(line.cost.totalAmount.amount), 0);
+      // 根据总金额找到对应的条件
+      const condition = findObjectByValue(conditions, buysTotalAmount, 'amount');
+      if (!condition.amount) return EMPTY_DISCOUNT; // 如果条件不满足金额要求，返回空折扣
+      return applyDiscount(condition, cartLines); // 应用折扣
+    }
+  }
+
+  if (buys.type === 'COLLECTIONS') {
+    const allGifts = getAllGiftVariants(conditions);
+    // 筛选购物车中不包含礼品且在给定集合中的商品
+    const excludedGiftLines = cartLines.filter(line => (line.product.inCollection && !allGifts.includes(removeGidStr(line.merchandise.id))));
+
+    if (rule === 'QUANTITY') {
+      // 根据筛选结果找到对应的条件
+      const condition = findObjectByValue(conditions, excludedGiftLines.length);
+      if (!condition.quantity) return EMPTY_DISCOUNT; // 如果条件不满足数量要求，返回空折扣
+      return applyDiscount(condition, cartLines); // 应用折扣
+    }
+
+    if (rule === 'AMOUNT') {
+      // 计算购物车中不包含礼品且在给定集合中的商品的总金额
+      const buysTotalAmount = excludedGiftLines
+        .reduce((total, line) => total + Number(line.cost.totalAmount.amount), 0);
+
+      // 根据总金额找到对应的条件
+      const condition = findObjectByValue(conditions, buysTotalAmount, 'amount');
+      if (!condition.amount) return EMPTY_DISCOUNT; // 如果条件不满足金额要求，返回空折扣
+      return applyDiscount(condition, cartLines); // 应用折扣
+    }
+  }
+
+  if (buys.type === 'TAGS') {
+    const allGifts = getAllGiftVariants(conditions);
+    // 筛选购物车中不包含礼品且包含给定tag的商品
+    const excludedGiftLines = cartLines.filter(line => (line.product.inTags && !allGifts.includes(removeGidStr(line.merchandise.id))));
+
+    if (rule === 'QUANTITY') {
+      // 根据筛选结果找到对应的条件
+      const condition = findObjectByValue(conditions, excludedGiftLines.length);
+      if (!condition.quantity) return EMPTY_DISCOUNT; // 如果条件不满足数量要求，返回空折扣
+      return applyDiscount(condition, cartLines); // 应用折扣
+    }
+
+    if (rule === 'AMOUNT') {
+      // 计算购物车中不包含礼品且在给定集合中的商品的总金额
+      const buysTotalAmount = excludedGiftLines
+        .reduce((total, line) => total + Number(line.cost.totalAmount.amount), 0);
+
       // 根据总金额找到对应的条件
       const condition = findObjectByValue(conditions, buysTotalAmount, 'amount');
       if (!condition.amount) return EMPTY_DISCOUNT; // 如果条件不满足金额要求，返回空折扣
